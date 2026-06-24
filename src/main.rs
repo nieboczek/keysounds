2ce98dfb10e814a028acbee36c341fc6418e90dd
nieboczek <bartekkoraliki@gmail.com>
2026-06-24 17:28:52 +0200

    feat: upgrade ratatui to 0.30

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
-use ratatui::Terminal;
-use ratatui::backend::CrosstermBackend;
-use ratatui::crossterm::execute;
-use ratatui::crossterm::terminal::{
-    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
-};
 use std::io;
-use std::io::Stdout;
 
 pub mod app;
 pub mod hotkey_handler;
 
-fn init_terminal() -> io::Result<Terminal<CrosstermBackend<Stdout>>> {
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    Terminal::new(CrosstermBackend::new(stdout))
-}
-
 fn main() -> io::Result<()> {
     let action_channel = hotkey_handler::start();
-    let mut terminal = init_terminal()?;
+    let mut terminal = ratatui::try_init()?;
     let mut app = app::App::new(action_channel);
 
     app.run(&mut terminal)?;
 
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-    terminal.show_cursor()
+    let _ = terminal.show_cursor();
+    ratatui::try_restore()
 }
